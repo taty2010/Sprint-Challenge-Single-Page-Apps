310fef9e746aa7a6fe6824fb63424ae118c12d3d
taty2010 <shydalys123@gmail.com>
2019-12-14 11:37:55 -0600

    added useEffect to get data from api and mapped over characters

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard"
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -7,11 +9,20 @@ export default function CharacterList() {
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.
+    get('https://rickandmortyapi.com/api/character/')
+    .then(response =>{
+      setCharacter(response.data.results)
+      console.log(response.data.results)
+    })
+    .catch(error => {console.log('error')})
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>{character.map(char =>{
+        return <CharacterCard name={char.name} />
+      })}</h2>
     </section>
   );
 }
