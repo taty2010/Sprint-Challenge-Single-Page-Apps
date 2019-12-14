aae20433990a068934f6982d69a554290978380d
taty2010 <shydalys123@gmail.com>
2019-12-14 11:58:04 -0600

    added nav for the header and imported Route and Link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Header from "./components/Header.js";
+import {Route, Link} from "react-router-dom"
 import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage.js";
 
 
 export default function App() {
   return (
     <main>
+      <nav>
       <Header />
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/character-list">Characters</Link>
+      </div>
+      </nav>
+      <WelcomePage />
       <CharacterList />
     </main>
   );
